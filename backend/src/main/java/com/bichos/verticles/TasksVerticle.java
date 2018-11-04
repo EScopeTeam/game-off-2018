2084b2aebb95cb287f3924ae45cbc66e72931796
rogelio-o <yo@rogelioorts.com>
2018-11-04 11:07:50 +0100

    Adds mechanism to get worker tasks from guice.

--- backend/src/main/java/com/bichos/verticles/TasksVerticle.java
+++ backend/src/main/java/com/bichos/verticles/TasksVerticle.java
@@ -1,7 +1,11 @@
 package com.bichos.verticles;
 
+import java.util.Set;
+
 import com.bichos.binders.GuiceInitializer;
+import com.bichos.tasks.WorkerTask;
 import com.google.inject.Injector;
+import com.google.inject.Key;
 
 import io.vertx.core.AbstractVerticle;
 
@@ -13,10 +17,22 @@ public class TasksVerticle extends AbstractVerticle {
   public void start() {
     initializeGuice();
 
+    runTasks();
   }
 
   private void initializeGuice() {
     injector = GuiceInitializer.initialize(vertx, config());
+  }
+
+  private void runTasks() {
+    for (final WorkerTask task : getWorkerTasks()) {
+      task.run(vertx, config());
+    }
+  }
+
+  private Set<WorkerTask> getWorkerTasks() {
+    return injector.getInstance(new Key<Set<WorkerTask>>() {
+    });
   }
 
 }
