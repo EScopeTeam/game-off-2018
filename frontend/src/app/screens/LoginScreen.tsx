ca44f461acf3a886c0eac963a9a9ef21b80d90cc
JORGE MARTIN LOPES <jorgemartin@viajeseci.es>
2018-11-12 08:45:05 +0100

    Add login screen

--- frontend/src/app/screens/LoginScreen.tsx
+++ frontend/src/app/screens/LoginScreen.tsx
@@ -1,10 +1,19 @@
 import React from "react";
-import { Button, View, Text } from "react-native";
+import {
+  Text,
+  View,
+  StyleSheet,
+  TextInput,
+  Image,
+  ImageBackground,
+} from 'react-native';
+import { Card, Button, Divider } from 'react-native-elements';
 import {
   NavigationScreenProp,
   NavigationScreenOptions,
 } from "react-navigation";
 import { navigationStyles } from "./../config/globalStyles";
+
 
 interface IProp {
   navigation: NavigationScreenProp<any, any>;
@@ -20,10 +29,54 @@ export default class LoginScreen extends React.Component<IProp, {}> {
     const navigation = this.props.navigation;
 
     return (
-      <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
-        <Text>Login</Text>
-        <Button title="SIGNUP" onPress={() => navigation.navigate("Signup")} />
+      <View style={styles.body}>
+        <ImageBackground
+          source={require('./assets/Rain.png')}
+          style={{ width: '100%', height: '100%' }}
+          resizeMode="repeat">
+          <Image style={styles.logo} />
+          <Card>
+            <TextInput defaultValue="Insert your user" />
+
+            <Divider style={{ backgroundColor: 'blue' }} />
+
+            <TextInput defaultValue="Insert your password" />
+          </Card>
+          <View style={styles.footer}>
+            <View
+              style={{
+                flexDirection: 'row',
+                justifyContent: 'space-around',
+              }}>
+              <Card flexDirection= "row" containerStyle={{ padding: 0 }}>
+                <Button large style={{margin:0}} title="Sign In" backgroundColor="#389798" />
+              </Card>
+              <Button large title="Sign Up" backgroundColor="#389798" onPress={() => navigation.navigate("Signup")} />
+              
+            </View>
+            <View
+              style={{
+                flex: 3,
+              }}
+            />
+          </View>
+        </ImageBackground>
       </View>
     );
   }
 }
+
+const styles = StyleSheet.create({
+  logo: {
+    flex: 2,
+    justifyContent: 'center',
+    padding: 8,
+  },
+  body: {
+    flex: 1,
+    backgroundColor: '#4cd4cc',
+  },
+  footer: {
+    flex: 2,
+  },
+});
